afd2d62e840e8a3584337ba4be9788ad25f6fa6b
renzon <renzon@gmail.com>
2016-05-20 12:34:44 -0300

    Exemplo de uso de módulo e __name__

---
+++ pacote/modulo.py
@@ -0,0 +1,4 @@
+from pacote2.modulo2 import para_float as p
+
+print('Hello Word')
+print(p(9))
---
+++ pacote2/modulo2.py
@@ -0,0 +1,6 @@
+def para_float(n):
+    return float(n)
+
+if __name__ == '__main__':
+    print(para_float(3))
+    print(__name__)
