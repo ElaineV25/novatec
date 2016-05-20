108ad5cd7988dc03f7b8c2aabcf28393b1d04da9
renzon <renzon@gmail.com>
2016-05-20 14:54:18 -0300

    lista e tupla

--- pacote/tipos_embutidos.py
+++ pacote/tipos_embutidos.py
@@ -1,5 +1,6 @@
-lista = [1, 2, 3, 4, 5]
+# Lista
 
+lista = [1, 2, 3, 4, 5]
 
 for valor in lista:
     print(valor)
@@ -8,8 +9,7 @@ lista.append(7)
 print(lista)
 print(list(range(10)))
 
-
-frutas='banana melancia abacate maça'.split(' ')
+frutas = 'banana melancia abacate maça'.split()
 print(frutas)
 
 # Slicing - Fatiamento
@@ -26,3 +26,24 @@ print(frutas[::2])
 
 print(list(reversed(frutas)))
 print(list(reversed(frutas[::-1])))
+del frutas[1]
+frutas.pop()
+frutas[0] = 'Banana Nanica'
+
+print(frutas)
+
+# Tuplas
+
+naipes = ('Paus', 'Ouros', 'Copas', 'Espadas')
+
+print(naipes)
+# naipes[0]='Foo' Operação proibida pois tupla é imutável
+
+print(naipes[:2])  # posso fazer pq cria nova tupla
+
+primeiro, *ultimos = naipes
+
+print(primeiro, ultimos)
+
+# Conjuntos
+
