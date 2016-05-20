b33f1c69989c563e56716022166a350cc4a6fd54
renzon <renzon@gmail.com>
2016-05-20 16:13:22 -0300

    Modificando docsttring

--- pacote/funcoes.py
+++ pacote/funcoes.py
@@ -2,22 +2,31 @@ def f(base, expoente=3, valor_resto=2):
     return (base ** expoente) % valor_resto
 
 
-print(f(3, 4))
-print(f(2))
-print(f(3, 4, 6))
-print(f(valor_resto=3, base=7))
+# print(f(3, 4))
+# print(f(2))
+# print(f(3, 4, 6))
+# print(f(valor_resto=3, base=7))
 
 
 def media(*numeros):
     '''
     Calcula a média aritmética dos argumentos da função
     Ou seja, soma os elementos e divide pelo numero total de elementos
+    :param numeros: parametros a serem extraidos da média
     :return: Número com média
     '''
-    return numeros
+    n = len(numeros)
+    if n == 0:
+        return 0
+
+    md = 0
+    for i in numeros:
+        md += i
+    return md / n
 
 
-print(media())
-print(media(1))
-print(media(1, 2))
-print(media(1, 2, 3))
+if __name__ == '__main__':
+    print(media())
+    print(media(1))
+    print(media(1, 2))
+    print(media(1, 2, 3))
