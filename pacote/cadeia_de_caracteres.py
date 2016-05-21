19886707c133249bba91f693b68a4313cfafbb1c
renzon <renzon@gmail.com>
2016-05-21 11:07:47 -0300

    entrada e saída de arquivos

--- pacote/cadeia_de_caracteres.py
+++ pacote/cadeia_de_caracteres.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 
 import logging
 import sys
+from io import UnsupportedOperation
 
 print(sys.getdefaultencoding())  # enconding utilizado pelo interpretador Python
 print(sys.getfilesystemencoding())  # encoding do sistema de arquivos
@@ -18,6 +19,27 @@ except FileNotFoundError as error:
     logging.error(error.strerror)
 
 with open('string.txt', mode='a', encoding='utf8') as arquivo:
-    nome_com_indice = 'Renzo Nuccitelli {}'.format(indice)
+    nome_com_indice = '{indice} Renzo Nuccitelli '.format(indice=indice)
     arquivo.write(nome_com_indice)
     arquivo.write('\n')
+
+arquivo = open('binario.bin', mode='rb')
+try:
+    cadeia_de_bytes = arquivo.read()
+    cadeia_total = bytearray()
+    for b in cadeia_de_bytes:
+        print(b)
+        cadeia_total.append(b)
+
+    print(cadeia_total)
+    print(cadeia_total.decode('cp1252'))
+    print(type(cadeia_total))
+except UnsupportedOperation:
+    logging.warning('Arquivo Binário não encontrado')
+finally:
+    arquivo.close()
+
+arquivo = open('binario.bin', mode='ab')
+
+arquivo.write('café'.encode('cp1252'))
+arquivo.close()
